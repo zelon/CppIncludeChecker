ef986cc30c98e671631f97408065e66428b8855e
KIM, JINWOOK <zelonion@gmail.com>
2018-08-08 00:03:08 +0900

    Add project files.

---
+++ CppIncludeChecker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CppIncludeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
